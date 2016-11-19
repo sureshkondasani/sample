0af41f4bf68d316f697d2079c64cdf854cc362e6
sureshkondasani <sureshkondasani@gmail.com>
2016-11-19 19:24:21 +0530

    sample commit

---
+++ Practice/src/com/practice/suresh/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.practice.suresh;
+
+import java.util.ArrayList;
+public class ArrayListDemo
+{
+public static void main(String args[])
+{
+ArrayList arrayList=new ArrayList();
+arrayList.add(10);
+arrayList.add(20);
+arrayList.add(30);
+arrayList.add(40);
+arrayList.add(50);
+System.out.println("ArrayList:"+arrayList);
+}
+}
---
+++ Practice/src/com/practice/suresh/HashSetDemo.java
@@ -0,0 +1,19 @@
+package com.practice.suresh;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+       HashSet hashSet = new HashSet();
+       hashSet.add(10);
+       hashSet.add(20);
+       hashSet.add(30);
+       hashSet.add(40);
+       hashSet.add(50);
+       
+       System.out.println("HashSet:"+hashSet);
+    }
+
+}
