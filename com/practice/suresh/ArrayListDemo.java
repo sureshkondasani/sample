a72e440a31d5ac272411964bfdd88c73e0fff138
sureshkondasani <sureshkskjava@gmail.com>
2016-11-19 19:03:43 +0530

    ArrayListDemo

---
+++ com/practice/suresh/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.practice.suresh;
+
+public class ArrayListDemo
+{
+public static void main(String args[])
+{
+ArrayList arrayList=new ArrayList();
+arrayList.add(10);
+arrayList.add(20);
+arrayList.add(30);
+arrayList.add(40);
+arrayList.add(50);
+
+}
+}
